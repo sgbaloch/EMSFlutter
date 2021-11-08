7322afdf96c537632241de1aefaf13841cba1090
Sarfaraz <sgbaloch@gmail.com>
2021-11-08 01:30:27 +0000

    Started add employee screen

--- ems_flutter/lib/screens/add_employee.dart
+++ ems_flutter/lib/screens/add_employee.dart
@@ -1,3 +1,4 @@
+import 'package:emsflutter/resources/AppColors.dart';
 import 'package:flutter/material.dart';
 
 class AddEmployee extends StatefulWidget {
@@ -10,11 +11,21 @@ class AddEmployee extends StatefulWidget {
 class _AddEmployeeState extends State<AddEmployee> {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Center(
-          child: Text("add employees"),
+        body: SafeArea(
+          child: Container(
+            padding: const EdgeInsets.all(15),
+            child: Container(
+              height: 250,
+              padding: const EdgeInsets.all(10),
+              decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(15)),
+                  color: Colors.white
+              ),
+            ),
+          ),
         ),
       ),
     );
